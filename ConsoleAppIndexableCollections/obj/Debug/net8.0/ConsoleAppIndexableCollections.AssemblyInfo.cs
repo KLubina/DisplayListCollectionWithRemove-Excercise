305804f5e666712e67e747380e78e4b0eeebc0af
Kristian Lubina <kristian.lubina@outlook.com>
2025-04-16 10:23:02 +0200

    renaming

--- ConsoleAppIndexableCollections/obj/Debug/net8.0/ConsoleAppIndexableCollections.AssemblyInfo.cs
+++ ConsoleAppIndexableCollections/obj/Debug/net8.0/ConsoleAppIndexableCollections.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleAppIndexableCollections")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe731105b7da8a5f8b804f6f82483ac83df4535f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleAppIndexableCollections")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleAppIndexableCollections")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
